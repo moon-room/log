80efa823c2fb054cb190f48154a15e046b588abb
awitherow <au.witherow@gmail.com>
2017-09-18 11:15:10 +0200

    add modal toggle

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import Logs from './Logs/'
 import Entry from './Entry/'
 
 class App extends Component {
+  state = {
+    profileModalVisible: false,
+    consultingModalVisible: false,
+    settingsModalVisible: false,
+  }
+
+  toggleModal = which => {
+    switch (which) {
+      case "profile": this.setState({
+        profileModalVisible: !this.state.profileModalVisible
+      }); break;
+      case "consulting": this.setState({
+        consultingModalVisible: !this.state.consultingModalVisible
+      }); break;
+      case "settings": this.setState({
+        settingsModalVisible: !this.state.settingsModalVisible
+      }); break;
+      default: console.log("how did you get here?")
+    }
+  }
+
   render() {
     return (
       <div className="container">
 
-        <Header />
+        <Header toggleModal={this.toggleModal} />
 
         <div className="content">
 
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from "./logo.png"
 import avatar from "./avatar.png"
-import seo from "./seo.png"
+import consulting from "./consulting.png"
 import settings from "./settings.png"
 import './index.css'
 
 class Header extends Component {
     render() {
+        const { toggleModal } = this.props
         return (
             <div className="header">
                 <div className="logo-container">
@@ -14,9 +15,15 @@ class Header extends Component {
                     <h1>MOON LOG</h1>
                 </div>
                 <div className="settings-container">
-                    <img className="icon small" src={seo} alt="a moon" />
-                    <img className="icon small" src={settings} alt="a moon" />
-                    <img className="icon small" src={avatar} alt="a moon" />
+                    <button className="menu-item" onPress={() => toggleModal('consulting')}>
+                        <img className="icon small" src={consulting} alt="humans talking to eachother" />
+                    </button>
+                    <button className="menu-item" onPress={() => toggleModal('settings')}>
+                        <img className="icon small" src={settings} alt="a machine cog" />
+                    </button>
+                    <button className="menu-item" onPress={() => toggleModal('profile')}>
+                        <img className="icon small" src={avatar} alt="a moon" />
+                    </button>
                 </div>
             </div>
         );
