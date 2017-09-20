a5181f0f83e2c4d5f88727efad73541559da1e09
awitherow <au.witherow@gmail.com>
2017-09-20 15:35:06 +0200

    temp disable api endpoints that do not yet exist

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression')
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,18 +16,18 @@ app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API
-// Authentication
-app.post('/auth/login', AttemptLogin);
-app.post('/auth/signup', AttemptSignup);
-// Authentication middleware
-app.use('/api', require('./validation/auth-check'));
-// Postgres DB Routes
-app.get('/api/entries', getEntries);
-app.get('/api/entries/:id', getEntry);
-app.post('/api/entries', createEntry);
-app.put('/api/entries/:id', updateEntry);
-app.delete('/api/entries/:id', removeEntry);
+// // API
+// // Authentication
+// app.post('/auth/login', AttemptLogin);
+// app.post('/auth/signup', AttemptSignup);
+// // Authentication middleware
+// app.use('/api', require('./validation/auth-check'));
+// // Postgres DB Routes
+// app.get('/api/entries', getEntries);
+// app.get('/api/entries/:id', getEntry);
+// app.post('/api/entries', createEntry);
+// app.put('/api/entries/:id', updateEntry);
+// app.delete('/api/entries/:id', removeEntry);
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function (request, response) {
