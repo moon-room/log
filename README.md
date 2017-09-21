de881e78ffb78403190119624ec4e97780ab4b54
awitherow <au.witherow@gmail.com>
2017-09-21 09:56:33 +0200

    add simple docs, update footer link

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # moon-log
 
 based on the technologies at https://github.com/mars/heroku-cra-node
+
+## server
+
+## frontend
+
+```bash
+cd react-ui
+npm i
+npm run start
+```
--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -27,13 +27,7 @@ class App extends Component {
 
         </div>
         <div className="attribution">
-          <div>Icons made by 
-            <a href="http://www.freepik.com" rel="noopener noreferrer" title="Freepik">Freepik</a>
-            from
-            <a href="https://www.flaticon.com/" rel="noopener noreferrer" title="Flaticon">www.flaticon.com</a>
-            is licensed by
-            <a href="http://creativecommons.org/licenses/by/3.0/"  rel="noopener noreferrer" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
-          </div>
+          <div>Icons from <a href="https://www.flaticon.com/" rel="noopener noreferrer" title="Flaticon">www.flaticon.com</a></div>
         </div>
       </div>
     );
