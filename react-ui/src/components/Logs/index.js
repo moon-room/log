7e6432305bbc3fd8fd1822113c1634dd4d47602c
awitherow <au.witherow@gmail.com>
2017-09-26 16:14:33 +0200

    add basic paginatino

--- react-ui/src/components/Logs/index.js
+++ react-ui/src/components/Logs/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Pagination from "react-js-pagination";
 import "./index.css";
 
 import remarkable from "remarkable";
@@ -9,86 +10,133 @@ const md = new remarkable();
 
 class Logs extends Component {
   state = {
-    logsShown: false
+    currentPage: 1,
+    resultsPerPage: 2
+  };
+
+  handlePageChange = n => {
+    console.log(n);
+    if (n === this.state.currentPage) {
+      return;
+    }
+    this.setState({ currentPage: n });
+  };
+
+  setResultsPerPage = e => {
+    const value = e.target.value;
+    if (value === this.state.resultsPerPage) {
+      return;
+    }
+    this.setState({ resultsPerPage: Number(value) });
   };
 
   render() {
+    const { resultsPerPage, currentPage } = this.state;
+
     return (
       <div className="logs-container">
         <div className="title">
           <h2>Trade Journal</h2>
+          <div className="results-container">
+            <label>Results Per Page</label>
+            <select value={resultsPerPage} onChange={this.setResultsPerPage}>
+              <option value="1">1</option>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="25">25</option>
+            </select>
+          </div>
+          <Pagination
+            prevPageText="prev"
+            nextPageText="next"
+            firstPageText="first"
+            lastPageText="last"
+            hideDisabled
+            activePage={currentPage}
+            itemsCountPerPage={resultsPerPage}
+            totalItemsCount={entries.length}
+            pageRangeDisplayed={3}
+            onChange={this.handlePageChange}
+          />
         </div>
         <div className="notepad-view">
-          {entries.map(
-            (
-              {
-                date,
-                coin,
-                entry,
-                exit,
-                technical_analysis,
-                gut_feeling,
-                other_influences,
-                next_time_improve,
-                what_went_well,
-                unexpected
-              },
-              i
-            ) => (
-              <div className="entry" key={i}>
-                <div className="entry__header">
-                  <h3>
-                    {date}, {coin}
-                  </h3>
-                  <ul>
-                    <li>Investment: {entry}</li>
-                    <li>Exit Amount: {exit}</li>
-                    <li>Winnings: {exit - entry}</li>
-                  </ul>
-                </div>
-                <h4>What Technical Analysis did I use to enter this trade?</h4>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: md.render(technical_analysis)
-                  }}
-                  className="markdown-container"
-                />
-                <h4>What was my gut feeling on entering this trade?</h4>
-                <div
-                  dangerouslySetInnerHTML={{ __html: md.render(gut_feeling) }}
-                  className="markdown-container"
-                />
-                <h4>Did anything else influence my decision?</h4>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: md.render(other_influences)
-                  }}
-                  className="markdown-container"
-                />
-                <h4>What I will improve upon next time</h4>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: md.render(next_time_improve)
-                  }}
-                  className="markdown-container"
-                />
-                <h4>What went well</h4>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: md.render(what_went_well)
-                  }}
-                  className="markdown-container"
-                />
-                <h4>Did anything occur that was unexpected?</h4>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: md.render(unexpected)
-                  }}
-                  className="markdown-container"
-                />
-              </div>
+          {entries
+            .slice(
+              currentPage * resultsPerPage - resultsPerPage,
+              currentPage * resultsPerPage
             )
-          )}
+            .map(
+              (
+                {
+                  date,
+                  coin,
+                  entry,
+                  exit,
+                  technical_analysis,
+                  gut_feeling,
+                  other_influences,
+                  next_time_improve,
+                  what_went_well,
+                  unexpected
+                },
+                i
+              ) => (
+                <div className="entry" key={i}>
+                  <div className="entry__header">
+                    <h3>
+                      {date}, {coin}
+                    </h3>
+                    <ul>
+                      <li>Investment: {entry}</li>
+                      <li>Exit Amount: {exit}</li>
+                      <li>Winnings: {exit - entry}</li>
+                    </ul>
+                  </div>
+                  <h4>
+                    What Technical Analysis did I use to enter this trade?
+                  </h4>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: md.render(technical_analysis)
+                    }}
+                    className="markdown-container"
+                  />
+                  <h4>What was my gut feeling on entering this trade?</h4>
+                  <div
+                    dangerouslySetInnerHTML={{ __html: md.render(gut_feeling) }}
+                    className="markdown-container"
+                  />
+                  <h4>Did anything else influence my decision?</h4>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: md.render(other_influences)
+                    }}
+                    className="markdown-container"
+                  />
+                  <h4>What I will improve upon next time</h4>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: md.render(next_time_improve)
+                    }}
+                    className="markdown-container"
+                  />
+                  <h4>What went well</h4>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: md.render(what_went_well)
+                    }}
+                    className="markdown-container"
+                  />
+                  <h4>Did anything occur that was unexpected?</h4>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: md.render(unexpected)
+                    }}
+                    className="markdown-container"
+                  />
+                </div>
+              )
+            )}
         </div>
       </div>
     );
--- react-ui/src/data.js
+++ react-ui/src/data.js
@@ -17,5 +17,141 @@ export const entries = [
     what_went_well:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
     unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    date: moment().format("MMM Do"),
+    coin: "BTC",
+    entry: 0.2,
+    exit: 0.3,
+    technical_analysis:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    gut_feeling:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    other_influences:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    next_time_improve:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    what_went_well:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    date: moment().format("MMM Do"),
+    coin: "BTC",
+    entry: 0.3,
+    exit: 0.4,
+    technical_analysis:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    gut_feeling:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    other_influences:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    next_time_improve:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    what_went_well:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    date: moment().format("MMM Do"),
+    coin: "BTC",
+    entry: 0.4,
+    exit: 0.5,
+    technical_analysis:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    gut_feeling:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    other_influences:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    next_time_improve:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    what_went_well:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    date: moment().format("MMM Do"),
+    coin: "BTC",
+    entry: 0.5,
+    exit: 0.6,
+    technical_analysis:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    gut_feeling:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    other_influences:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    next_time_improve:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    what_went_well:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    date: moment().format("MMM Do"),
+    coin: "BTC",
+    entry: 0.6,
+    exit: 0.7,
+    technical_analysis:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    gut_feeling:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    other_influences:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    next_time_improve:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    what_went_well:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    date: moment().format("MMM Do"),
+    coin: "BTC",
+    entry: 0.7,
+    exit: 0.8,
+    technical_analysis:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    gut_feeling:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    other_influences:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    next_time_improve:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    what_went_well:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    date: moment().format("MMM Do"),
+    coin: "BTC",
+    entry: 0.8,
+    exit: 0.9,
+    technical_analysis:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    gut_feeling:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    other_influences:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    next_time_improve:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    what_went_well:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    date: moment().format("MMM Do"),
+    coin: "BTC",
+    entry: 0.9,
+    exit: 0.1,
+    technical_analysis:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    gut_feeling:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    other_influences:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    next_time_improve:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    what_went_well:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra pellentesque justo, in tempus est consequat quis.",
+    unexpected: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
   }
 ];
