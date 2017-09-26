56801917714da0264a0c6c140c17071c8424dce6
awitherow <au.witherow@gmail.com>
2017-09-26 14:54:33 +0200

    style attribution

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,33 +1,39 @@
-import React, { Component } from 'react';
-import Header from './components/Header/'
-import Logs from './components/Logs/'
-import Entry from './components/Entry/'
-import Overlay from './components/Overlay'
+import React, { Component } from "react";
+import Header from "./components/Header/";
+import Logs from "./components/Logs/";
+import Entry from "./components/Entry/";
+import Overlay from "./components/Overlay";
 
 class App extends Component {
   state = {
-    modalVisible: "",
-  }
+    modalVisible: ""
+  };
 
-  toggleModal = which => this.setState({ modalVisible: which })
+  toggleModal = which => this.setState({ modalVisible: which });
 
   render() {
-    const { modalVisible } = this.state
+    const { modalVisible } = this.state;
     return (
       <div className="container">
-
         <Overlay visible={modalVisible} toggleModal={this.toggleModal} />
 
         <Header toggleModal={this.toggleModal} />
 
         <div className="content">
-
           <Logs />
           <Entry />
-
         </div>
         <div className="attribution">
-          <div>Icons from <a href="https://www.flaticon.com/" rel="noopener noreferrer" title="Flaticon">www.flaticon.com</a></div>
+          <div>
+            Icons from{" "}
+            <a
+              href="https://www.flaticon.com/"
+              rel="noopener noreferrer"
+              title="Flaticon"
+            >
+              www.flaticon.com
+            </a>
+          </div>
         </div>
       </div>
     );
