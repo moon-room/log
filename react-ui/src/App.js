7a19aebf5d9236403c395c7df148ba7a0b1533be
awitherow <au.witherow@gmail.com>
2017-10-11 15:32:12 +0200

    fix redirect loop

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 import { isUserAuthenticated } from "./api/security";
 
@@ -22,8 +22,10 @@ export default class App extends Component {
     return (
       <BrowserRouter>
         <div id="App">
-          <Route exact pattern="/portal" component={Portal} />
-          <PrivateRoute exact pattern="/" component={MoonLog} />
+          <Switch>
+            <Route exact pattern="/portal" component={Portal} />
+            <PrivateRoute exact pattern="/" component={MoonLog} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
