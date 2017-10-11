f499e0112551274477d06990ed56214cfdf4c401
awitherow <au.witherow@gmail.com>
2017-10-11 16:49:43 +0200

    add dotenv for local development

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const path = require('path');
-const compression = require('compression')
-const bodyParser = require('body-parser')
+const express = require("express");
+const path = require("path");
+const compression = require("compression");
+const bodyParser = require("body-parser");
+
+if (process.env.ENV === "test") {
+  require("dotenv").config();
+}
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Compression for performance enhancement
-app.use(compression())
+app.use(compression());
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
 // Enable handling of REST
 app.use(bodyParser.json());
@@ -30,10 +34,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.delete('/api/entries/:id', removeEntry);
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+app.get("*", function(request, response) {
+  response.sendFile(path.resolve(__dirname, "../react-ui/build", "index.html"));
 });
 
-app.listen(PORT, function () {
-    console.log(`Listening on port ${PORT}`);
+app.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}`);
 });
