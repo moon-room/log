52c2bb5d4882fcb50e4892c24c7ff020a220c99c
awitherow <au.witherow@gmail.com>
2017-10-11 15:56:06 +0200

    add portal

--- react-ui/src/components/views/Portal.js
+++ react-ui/src/components/views/Portal.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import "./portal.css";
+import moon from "../../media/logo.png";
 
 export default class Portal extends Component {
   state = {
@@ -67,29 +69,43 @@ export default class Portal extends Component {
     return redirectToReferrer ? (
       <Redirect to="/" />
     ) : (
-      <div className="login-container">
-        <div className="form-group">
-          <label>Username: </label>
-          <input
-            id="username"
-            value={username}
-            onChange={e => this.setState({ username: e.target.value })}
-          />
-        </div>
+      <div className="portal">
+        <div className="login-container">
+          <div className="introduction">
+            <img className="logo" src={moon} alt="moon" />
+            <p className="description">
+              Welcome to the <strong>MOON LOG</strong>. Log your trades, reflect
+              on your actions, learn and grow. Fill in the form, click login if
+              you have an account, or register to make a new one.
+            </p>
+          </div>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              id="username"
+              value={username}
+              onChange={e => this.setState({ username: e.target.value })}
+            />
+          </div>
 
-        <div className="form-group">
-          <label>Password: </label>
-          <input
-            id="password"
-            value={password}
-            type="password"
-            onChange={e => this.setState({ password: e.target.value })}
-          />
-        </div>
+          <div className="form-group">
+            <label>Password: </label>
+            <input
+              id="password"
+              value={password}
+              type="password"
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </div>
 
-        <div className="button-container">
-          <button onClick={this.authenticateLoginAttempt}>Login</button>
-          <button onClick={this.authenticateSignupAttempt}>Signup</button>
+          <div className="button-container">
+            <button className="green" onClick={this.authenticateLoginAttempt}>
+              Login
+            </button>
+            <button className="blue" onClick={this.authenticateSignupAttempt}>
+              Signup
+            </button>
+          </div>
         </div>
       </div>
     );
